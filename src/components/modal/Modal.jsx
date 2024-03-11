7dcc1921075263dfa53f9d856e508308f71163eb
Bayz.ng <ayofe70@gmail.com>
2024-03-10 18:35:09 -0700

    change commit

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -79,7 +79,7 @@ export default function Modal({ name, address, pincode, phoneNumber, setName, se
 
                                                     </form>
                                                     {/* <button onClick={()=>{buyNow(); closeModal()}} type="button" className="focus:outline-none w-full text-white bg-violet-600 hover:bg-violet-800  outline-0 font-medium rounded-lg text-sm px-5 py-2.5 ">Order Now</button> */}
-                                                    <button type="button" className="focus:outline-none w-full text-white bg-violet-600 hover:bg-violet-800  outline-0 font-medium rounded-lg text-sm px-5 py-2.5 "><a href="https://wa.me/8144796373">Payment Checkout</a></button>
+                                                    <button type="button" className="focus:outline-none w-full text-white bg-violet-600 hover:bg-violet-800  outline-0 font-medium rounded-lg text-sm px-5 py-2.5 "><a href="https://wa.me/8111634621">Payment Checkout</a></button>
 
                                                 </div>
                                             </div>
